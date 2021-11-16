984aa3cd1640c6c8fa0b318ec57eda250005b815
MehmetTopall <mehmettopalofficial@gmail.com>
2021-11-16 15:42:27 +0300

    2-yorum

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -16,5 +16,6 @@ namespace Business.Constants
         public static string ProductDeleted = "Ürün silindi";
         public static string ProductNotDeletable = "Bu ürün silinemez";
         //bu branch için deneme yorumudur.
+        //Branch Deneme 2 yorumudur.
     }
 }
